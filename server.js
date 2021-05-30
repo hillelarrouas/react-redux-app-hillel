fb0d03b4dfd6cfa5d8da10aa7c82ec1d6b0d5e2f
hillel <a4105962@gmail.com>
2021-05-31 02:07:44 +0300

    cont

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express();
 
 
-app.use(express.static(path.join(__dirname + 'client/build')));
+app.use(express.static(path.join(__dirname , 'client/build')));
 
 
 const array = [
